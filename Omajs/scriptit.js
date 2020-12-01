e743b7de5010549da3eff7c8b53f023e1fcf7677
JoonasHalttu <Joonas.halttu@edu.salpaus.fi>
2020-12-01 10:47:13 +0000

    2020121

---
+++ Omajs/scriptit.js
@@ -0,0 +1,11 @@
+
+const etunimi='Joonas';//string
+const sukunimi='Halttu';//string
+const ika=16
+const lahtelainen=true;//boolean
+let maarittelematon; 
+
+console.log(etunimi + ' ' + ika);
+
+const opiskelija=true;
+console.log("opiskelija tyyppi: " + typeof opiskelija);
